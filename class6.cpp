ee97d3f35699ada47aa5b895f67b8dd571d90661
SayanjitDas-web <thewebman01@gmail.com>
2025-02-27 10:43:14 +0530

    structure added

--- class6.cpp
+++ class6.cpp
@@ -1,7 +1,33 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
+struct Student{
+   string studentName;
+   int age;
+   string stream;
+   int rollNo;
+};
+
+
+
 int main(){
+    Student s1;
+    s1.studentName = "Student1";
+    s1.age = 22;
+    s1.stream = "food tech";
+    s1.rollNo = 12;
+
+    cout << s1.studentName << endl;
     return 0;
 }
+
+// https://github.com/SayanjitDas-web/cppClassGNIT
+
+// structure :-
+// 1. can be manipulated;
+// 2. can contain multiple types;
+// 3. it can hold functions;
+// 4. by default all members are public;
+// 5. each members hold separate memory space;
