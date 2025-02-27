2d3f0affdbe52d2d43991d753ca89f092a1cc2b1
SayanjitDas-web <thewebman01@gmail.com>
2025-02-27 11:38:22 +0530

    updated code added

--- class6.cpp
+++ class6.cpp
@@ -16,9 +16,28 @@ union Data{
   char val3;
 };
 
+//enumeration
+enum Colour{
+    RED = true,
+    BLUE = 200,
+    YELLOW = 300,
+    GREEN = 400
+};
+
+typedef unsigned int uint;
+typedef string str;
+
 int main(){
+    float fl = 23.33;
+    int inte = (int) fl;
+    str str1 = "this is string";
+    cout << inte << endl;
+    uint num = -22;
+    cout << num << endl;
     Student s1,s2;
     Data d;
+    Colour c = GREEN;
+    cout << c << endl;
     s1.studentName = "Student1";
     s1.age = 22;
     s1.stream = "food tech";
